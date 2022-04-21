ae8fc5817109fb63ef7756c054c838cb9048a4c7
jc366512 <jclouse@albany.edu>
2022-04-21 12:14:13 -0400

    created file to query api (using python)

---
+++ query_API.py
@@ -0,0 +1,27 @@
+# This program is going to interact with the public virustotal api and get requests back
+# To ensure security, I will ask the user to input their API key from the bash terminal (or from a seperat file that is not tracked)
+
+# -=-=-=-=-
+# Imports
+# -=-=-=-=-
+import requests
+import time 
+import json
+
+# -=-=-=-=-
+# Functions
+# -=-=-=-=-
+
+
+
+# -=-=-=-=-
+# Variables
+# -=-=-=-=-
+
+
+
+# -=-=-=-=-
+#
+# MAIN
+# 
+# -=-=-=-=-
